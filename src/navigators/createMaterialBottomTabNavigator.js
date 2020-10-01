08d20b4af84b131bdc52b9ae081b60ea4f878f00
Abhishek Bharadwaj <ab.bharadwaj03@gmail.com>
2020-10-01 13:06:30 +0530

    Added send event for tab callback

--- src/navigators/createMaterialBottomTabNavigator.js
+++ src/navigators/createMaterialBottomTabNavigator.js
@@ -1,20 +1,15 @@
 /* @flow */
-
-import * as React from 'react';
-import BottomNavigation from 'react-native-paper/src/components/BottomNavigation';
-import { createTabNavigator } from 'react-navigation-tabs';
-import {withTheme} from "react-native-paper";
-
-
+import * as React from "react";
+import BottomNavigation from "react-native-paper/src/components/BottomNavigation";
+import { createTabNavigator } from "react-navigation-tabs";
+import { withTheme } from "react-native-paper";
 class BottomNavigationView extends React.Component {
   _getColor = ({ route }) => {
     const { descriptors } = this.props;
     const descriptor = descriptors[route.key];
     const options = descriptor.options;
-
     return options.tabBarColor;
   };
-
   _isVisible() {
     const { navigation, descriptors } = this.props;
     const { state } = navigation;
@@ -22,11 +17,9 @@ class BottomNavigationView extends React.Component {
     const options = descriptors[route.key].options;
     return options.tabBarVisible;
   }
-
   _renderIcon = ({ route, focused, color }) => {
     return this.props.renderIcon({ route, focused, tintColor: color });
   };
-
   render() {
     const {
       activeTintColor,
@@ -36,28 +29,31 @@ class BottomNavigationView extends React.Component {
       descriptors,
       barStyle,
       theme,
+      sendEventForTab,
       ...rest
     } = this.props;
-
     const isVisible = this._isVisible();
     const extraStyle =
-      typeof isVisible === 'boolean'
-        ? { display: isVisible ? null : 'none' }
+      typeof isVisible === "boolean"
+        ? { display: isVisible ? null : "none" }
         : null;
-
     return (
       <BottomNavigation
         // Pass these for backward compaibility
         {...rest}
         activeColor={theme.colors.activeColor}
-        inactiveColor={theme.colors.inactiveColor} 
+        inactiveColor={theme.colors.inactiveColor}
         renderIcon={this._renderIcon}
-        barStyle={[barStyle, extraStyle,{backgroundColor: theme.colors.bg}]}
+        barStyle={[barStyle, extraStyle, { backgroundColor: theme.colors.bg }]}
         navigationState={navigation.state}
         getColor={this._getColor}
+        onIndexChange={(index) => {
+          if (sendEventForTab) {
+            sendEventForTab(navigation.state, index);
+          }
+        }}
       />
     );
   }
 }
-
 export default createTabNavigator(withTheme(BottomNavigationView));
